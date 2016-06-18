84cc642e190ee42ebc4a23c54c9b5a2764df57e3
Alison Appling <aappling@usgs.gov>
2016-06-17 19:02:13 -0500

    make authRemote work more like other generics in vizlab

--- R/authRemote.R
+++ R/authRemote.R
@@ -9,37 +9,34 @@
 #' e.g., "auth.sciencebase.R". Place this script in the user/local or 
 #' user/docker folder (depending on whether you will be building the 
 #' visualization directly or via docker) (BUT docker-specific authentication 
-#' isn't yet implemented. stay tuned). This script should log on to the service
-#' with valid credentials. Take care to never commit the user directory to an
+#' isn't yet implemented. stay tuned). This script should log on to the service 
+#' with valid credentials. Take care to never commit the user directory to an 
 #' unsecured repository.
 #' 
 #' @param fetcher the name of the fetcher for which to authenticate, e.g., 
 #'   'sciencebase'
+#' @param ... other arguments passed to authRemote methods
 #' @examples 
+#' \dontrun{
 #' # An example 1-line user/local/auth.sciencebase.R (fill in the credentials):
 #' sbtools::authenticate_sb(myusername, mypassword)
+#' }
 #' @export
 authRemote <- function(fetcher, ...) UseMethod("authRemote")
 
-#' @export
-authRemote.default <- function(fetcher, ...) {
-  # give error if fetcher doesn't specify a defined methods (otherwise the error
-  # is infinite recursion, which is confusing)
-  knownMethods <- setdiff(sapply(strsplit(as.character(methods('authRemote')), '\\.'), `[[`, 2), 'default')
-  if(!(fetcher %in% knownMethods)) stop('unknown fetcher')
-  
-  # route the method to the specified fetcher
-  class(fetcher) <- fetcher
-  invisible(authRemote(fetcher, user='local')) # how to know whether this should be local or docker?
-}
-
-#' \code{authRemote.file} currently does nothing. It exists to communicate that 
-#' the .file type is an option for data items.
-#' 
+#' @param user not yet well thought out, but the idea is to accept 'local' or 
+#'   'docker' and use the corresponding user profile to authenticate
+#'   
 #' @rdname authRemote
 #' @export
-authRemote.file <- function(fetcher, user) {
-  invisible()
+authRemote.default <- function(fetcher, user='local', ...) {
+  # explain the problem if we're headed for infinite recursion
+  if(class(fetcher) != 'character') 
+    stop('could not find authRemote method for fetcher=', fetcher)
+
+  # route the method to the specified fetcher
+  class(fetcher) <- fetcher
+  invisible(authRemote(fetcher, user=user, ...))
 }
 
 #' \code{authRemote.sciencebase} calls the user's script at 
@@ -49,7 +46,7 @@ authRemote.file <- function(fetcher, user) {
 #' 
 #' @rdname authRemote
 #' @export
-authRemote.sciencebase <- function(fetcher, user) {
+authRemote.sciencebase <- function(fetcher, user, ...) {
   auth.script <- paste0("user/", user, "/auth.sciencebase.R")
   if(file.exists(auth.script)) {
     source(auth.script)
