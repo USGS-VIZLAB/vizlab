7ac0e301fafe866050a4ac6131715d008be7c986
Alison Appling <aappling@usgs.gov>
2016-06-19 14:37:35 -0500

    don't overwrite viz.yaml in vizSkeleton

--- R/vizSkeleton.R
+++ R/vizSkeleton.R
@@ -22,6 +22,7 @@ vizSkeleton <- function (name = "cool-viz", path = ".") {
   safe.dir.create(path)
   safe.dir.create(file.path(path, "data"))
   safe.dir.create(file.path(path, "images"))
+  safe.dir.create(file.path(path, "figures"))
   safe.dir.create(layout_dir <- file.path(path, "layout"))
   safe.dir.create(file.path(layout_dir, "css"))
   safe.dir.create(file.path(layout_dir, "js"))
@@ -32,26 +33,30 @@ vizSkeleton <- function (name = "cool-viz", path = ".") {
   safe.dir.create(file.path(script_dir, "visualize"))
   safe.dir.create(file.path(script_dir, "read"))
 
-  message("Creating viz.yaml ...", domain = NA)
-  viz.yaml <- file(file.path(path, "viz.yaml"))
-  cat("vizlab: ", "\"", as.character(packageVersion(getPackageName())), "\"\n",
-      "info:\n",
-      "  id: ", name, "\n",
-      "  name: ", "Visualization long name","\n",
-      "  date: ", format(Sys.Date(), "%Y-%m-%d"), "\n",
-      "  publish-date: ", format(Sys.Date() + 14, "%Y-%m-%d"), "\n",
-      "  path: ", "/", name, "\n",
-      "  description: >-\n",
-      "    Describe project here\n",
-      "pages:\n",
-      "js:\n",
-      "css:\n",
-      "sections:\n",
-      "fetch:\n",
-      "process:\n",
-      "visualize:\n",
-      file = viz.yaml, sep = "")
-  close(viz.yaml)
-
+  if(file.exists('viz.yaml')) {
+    message("viz.yaml already exists; leaving as-is")
+  } else {
+    message("Creating viz.yaml ...", domain = NA)
+    viz.yaml <- file(file.path(path, "viz.yaml"))
+    cat("vizlab: ", "\"", as.character(packageVersion(getPackageName())), "\"\n",
+        "info:\n",
+        "  id: ", name, "\n",
+        "  name: ", "Visualization long name","\n",
+        "  date: ", format(Sys.Date(), "%Y-%m-%d"), "\n",
+        "  publish-date: ", format(Sys.Date() + 14, "%Y-%m-%d"), "\n",
+        "  path: ", "/", name, "\n",
+        "  description: >-\n",
+        "    Describe project here\n",
+        "pages:\n",
+        "js:\n",
+        "css:\n",
+        "sections:\n",
+        "fetch:\n",
+        "process:\n",
+        "visualize:\n",
+        file = viz.yaml, sep = "")
+    close(viz.yaml)
+  }
+  
   # Read-and-delete-me type file would be good to describe next steps
 }
