92f080d3d1bcd89d53f752213cfbe9a4481389be
Alison Appling <aappling@usgs.gov>
2016-06-19 14:37:54 -0500

    default args for demo custom visualizer

--- inst/testviz/scripts/visualize/relativeAbundance.R
+++ inst/testviz/scripts/visualize/relativeAbundance.R
@@ -2,7 +2,11 @@
 #' 
 #' Could also accept these args: viz.id, data.info. but we're not using them so
 #' won't list them.
-visualizeData.relativeAbundance <- function(CuyahogaShort, MayflyNymph, ..., outfile) {
+visualizeData.relativeAbundance <- function(
+  CuyahogaShort=readData('CuyahogaShort'), 
+  MayflyNymph=readData('MayflyNymph'), 
+  outfile='figures/relativeAbundanceFig.svg', ...) {
+  
   svg(outfile, height=4, width=4)
   plot(Above ~ Below, MayflyNymph)
   dev.off()
