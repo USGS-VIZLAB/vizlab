9b28ec153e7dc1013a345349e8221bf2493c6bf7
Jordan Walker <jiwalker@usgs.gov>
2016-09-20 18:23:37 -0500

    Using xml2 to add attributes

--- R/analytics.R
+++ R/analytics.R
@@ -26,31 +26,42 @@ analytics.analytics <- function(viz) {
 #' Add a trigger for a given element to cause a analytics event
 #' when it is scrolled to
 #'
-#' @importFrom htmltools as.tags HTML tagAppendAttributes
+#' @importFrom xml2 read_xml xml_has_attr xml_attr xml_attr<-
 #' @rdname analytics
 #' @export
 analytics.scroll <- function(viz) {
   required <- c("output")
   checkRequired(viz, required)
 
-  tags <- as.tags(HTML(viz[['output']]))
-  tags <- tagAppendAttributes(tags, class = "vizScroll")
-  viz[['output']] <- print(tags, browse = FALSE)
+  class <- "vizScroll"
+  html <- read_xml(viz[['output']])
+  if (xml_has_attr(html, "class")) {
+    xml_attr(html, "class") <- paste(xml_attr(html, "class"), class)
+  } else {
+    xml_attr(html, "class") <- class
+  }
+  viz[['output']] <- format(html)
   return(viz)
 }
 
 #' Add an event handler for a user clicking on a visualization
 #' that causes an analytics event to occur
-#' @importFrom htmltools as.tags HTML tagAppendAttributes
+#'
+#' @importFrom xml2 read_xml xml_has_attr xml_attr xml_attr<-
 #' @rdname analytics
 #' @export
 analytics.click <- function(viz) {
   required <- c("output")
   checkRequired(viz, required)
 
-  tags <- as.tags(HTML(viz[['output']]))
-  tags <- tagAppendAttributes(tags, class = "vizClick")
-  viz[['output']] <- print(tags, browse = FALSE)
+  class <- "vizClick"
+  html <- read_xml(viz[['output']])
+  if (xml_has_attr(html, "class")) {
+    xml_attr(html, "class") <- paste(xml_attr(html, "class"), class)
+  } else {
+    xml_attr(html, "class") <- class
+  }
+  viz[['output']] <- format(html)
   return(viz)
 }
 
--- tests/testthat/tests-analytics.R
+++ tests/testthat/tests-analytics.R
@@ -11,3 +11,15 @@ test_that("scroll class is added to output", {
   viz <- analytics(viz)
   expect_match(viz[['output']], "class=\"vizScroll\"")
 })
+
+test_that("click class is added to output", {
+  viz <- as.viz(list(
+    id = "click-test",
+    location = "test.html",
+    output = "<div id=\"click-test\" class=\"hidden\"><a href=\"/test\"></a></div>",
+    publisher = "section",
+    analytics = "click"
+  ))
+  viz <- analytics(viz)
+  expect_match(viz[['output']], "class=\"hidden vizClick\"")
+})
