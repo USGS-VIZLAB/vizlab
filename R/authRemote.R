e6692dd58c36b82e7ca9fb3492255afcb2330be9
wdwatkins <wwatkins@usgs.gov>
2016-09-22 10:50:08 -0500

    updated documentation; should be good to go

--- R/authRemote.R
+++ R/authRemote.R
@@ -1,19 +1,10 @@
 #' Log on to a remote file service
 #' 
-#' TODO: update documentation
-#' 
-#' Uses authentication information in the user directory to log onto a remote 
+#' Uses authentication information in the ~/.vizlab driectory to log onto a remote 
 #' file service such as ScienceBase. This function is primarily for internal use
 #' but can be called directly to check credentials.
 #' 
-#' To add credentials, create an R script called "auth.fetcher.R" where 
-#' "fetcher" is replaced by the specific fetcher as designated in viz.yaml, 
-#' e.g., "auth.sciencebase.R". Place this script in the user/local or 
-#' user/docker folder (depending on whether you will be building the 
-#' visualization directly or via docker) (BUT docker-specific authentication 
-#' isn't yet implemented. stay tuned). This script should log on to the service 
-#' with valid credentials. Take care to never commit the user directory to an 
-#' unsecured repository.
+#' To add ScienceBase credentials, run the \code{storeSBcreds} function.  
 #' 
 #' @param fetcher the name of the fetcher for which to authenticate, e.g., 
 #'   'sciencebase'
@@ -36,10 +27,9 @@ authRemote.default <- function(fetcher, user='local', ...) {
   invisible(authRemote(fetcher, user=user, ...))
 }
 
-#' \code{authRemote.sciencebase} calls the user's script at 
-#' user/local/auth.sciencebase.R. That script should contain the R commands and
-#' credentials to log on to ScienceBase with read (and possibly write)
-#' permissions.
+#' \code{authRemote.sciencebase} Uses the credentials stored in ~/.vizlab to log in to 
+#' ScienceBase.  Use the storeSBcreds function to verify and store credentials.  Since vizlab
+#' starts its own R session, an existing ScienceBase session cannot be used. 
 #' 
 #' @rdname authRemote
 #' @export
@@ -48,15 +38,15 @@ authRemote.sciencebase <- function(fetcher, user, ...) {
   home <- path.expand('~')
   sbCreds <- file.path(home, ".vizlab/sbCreds")
   
-  #check if already logged in 
-  if(sbtools::is_logged_in()){
+  #check if already logged in; don't think there is a way for this to happen
+  if(sbtools::is_logged_in()){ 
     message("Using existing sciencebase session")
   } else if(file.exists(sbCreds)) {
     credList <- readRDS(sbCreds)
     un <- rawToChar(credList$username)
     pw <- rawToChar(credList$password)
     sbtools::authenticate_sb(un, pw)
-    message("Logging into sciencebase with stored credentials")
+    message("Logging into ScienceBase with stored credentials")
   } else {
     message('requesting data from ScienceBase without authentication because   
             no credentials exist.  Use the storeSBcreds() function to add credentials.')
@@ -65,7 +55,8 @@ authRemote.sciencebase <- function(fetcher, user, ...) {
   invisible()
 }
 
-#' Store and verify credentials for sciencebase
+#' Store and verify credentials for ScienceBase.  User is prompted for credentials.
+#' Using a ScienceBase account tied to an AD account is not recommended for password security.
 #' @export
 #' 
 storeSBcreds<- function(){
