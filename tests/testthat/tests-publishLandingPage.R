3b2779d38c607eeed48a6aff6c9691ab655b6f2a
Lindsay Carr <lcarr@usgs.gov>
2016-10-05 11:17:04 -0500

    edit test based on updates to publishLandingPage

--- tests/testthat/tests-publishLandingPage.R
+++ tests/testthat/tests-publishLandingPage.R
@@ -2,12 +2,12 @@ context("publish viz landing page")
 
 test_that("publishLandingPage works", {
   
-  landing_path <- publishLandingPage(index_loc = tempdir())
-  index <- readLines(landing_path)
+  publishLandingPage()
+  index <- readLines('landing/target/index.html')
   
-  expect_true(any(grepl('USGS-VIZLAB/great-lakes-microplastics', index)))
-  expect_true(any(grepl('USGS-VIZLAB/climate-fish-habitat', index)))
-  expect_false(any(grepl('USGS-VIZLAB/example', index)))
-  expect_false(any(grepl('USGS-VIZLAB/vizlab', index)))
-  
+  expect_true(any(grepl('/microplastics', index)))
+  expect_true(any(grepl('/climatefish', index)))
+  expect_false(any(grepl('/example', index)))
+  expect_false(any(grepl('href=\"./vizlab', index)))
+
 })
