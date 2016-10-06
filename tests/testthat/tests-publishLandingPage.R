6c6086bad226d796c6e707b41191a9b121243dbd
Jordan Walker <jiwalker@usgs.gov>
2016-10-06 11:25:43 -0500

    adding cleanup for landing dir

--- tests/testthat/tests-publishLandingPage.R
+++ tests/testthat/tests-publishLandingPage.R
@@ -1,13 +1,19 @@
 context("publish viz landing page")
 
+cleanup <- function() {
+  dir <- "landing"
+  unlink(dir, recursive = TRUE)
+}
+
 test_that("publishLandingPage works", {
-  
+
   publishLandingPage()
   index <- readLines('landing/target/index.html')
-  
+
   expect_true(any(grepl('/microplastics', index)))
   expect_true(any(grepl('/climatefish', index)))
   expect_false(any(grepl('/example', index)))
   expect_false(any(grepl('href=\"./vizlab', index)))
 
+  cleanup()
 })
