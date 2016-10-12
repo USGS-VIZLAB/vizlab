31c58298ac77c2fa92f2064f548eee93a7f918a4
Jordan Walker <jiwalker@usgs.gov>
2016-10-12 11:40:00 -0500

    fixing test

--- tests/testthat/tests-publishLandingPage.R
+++ tests/testthat/tests-publishLandingPage.R
@@ -1,5 +1,8 @@
 context("publish viz landing page")
 
+oldwd <- getwd()
+setuptmp <- setup()
+
 test_that("publishLandingPage works", {
 
   publishLandingPage()
@@ -9,7 +12,6 @@ test_that("publishLandingPage works", {
   expect_true(any(grepl('/climate-change-walleye-bass', index)))
   expect_false(any(grepl('/example', index)))
   expect_false(any(grepl('href=\"./vizlab', index)))
-
 })
 
 cleanup(oldwd, setuptmp)
