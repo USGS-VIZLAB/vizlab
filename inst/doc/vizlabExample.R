db55b65e2dcffad33b97390ff8883e8bf170d2c7
wdwatkins <wwatkins@usgs.gov>
2016-10-18 10:11:21 -0500

    vignette setup

---
+++ inst/doc/vizlabExample.R
@@ -0,0 +1,7 @@
+## ----setup, include=FALSE------------------------------------------------
+library(rmarkdown)
+
+## ----eval=FALSE----------------------------------------------------------
+#  install.packages('devtools')
+#  devtools::install.github('USGS-VIZLAB/vizlab')
+
