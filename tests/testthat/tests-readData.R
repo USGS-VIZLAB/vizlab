9f4d5a293f5efe9ad9c5831a29b6f18c3f939ce3
Jordan S Read <jread@usgs.gov>
2016-11-23 09:37:07 -0600

    for #91 and #104

---
+++ tests/testthat/tests-readData.R
@@ -0,0 +1,38 @@
+
+
+oldwd <- getwd()
+#these tests need to use the test viz
+testtmp <- setup(copyTestViz=TRUE)
+viz <- as.viz(list(
+  depends = c("MayflyNymph", "carData")
+))
+
+context("readData")
+
+test_that("readData works as expected", {
+  
+  mayfly <<- readData(viz[['depends']][1])
+  carData <<- readData(viz[['depends']][2])
+  expect_is(mayfly, 'data.frame')
+  expect_is(carData, 'list')
+  expect_is(carData[[1]], 'data.frame')
+  expect_is(carData[[2]], 'data.frame')
+})
+
+context("readDepends")
+test_that("readDepends works as expected with viz input",{
+  viz.dep <- readDepends(viz)
+  expect_equal(mayfly, viz.dep[["MayflyNymph"]])
+  expect_equal(viz.dep[["carData"]], carData)
+})
+
+
+test_that("readDepends works as expected with list input",{
+  viz.dep <- readDepends(list(
+    depends = c("MayflyNymph", "carData")
+  ))
+  expect_equal(mayfly, viz.dep[["MayflyNymph"]])
+  expect_equal(viz.dep[["carData"]], carData)
+})
+
+cleanup(oldwd, testtmp)
