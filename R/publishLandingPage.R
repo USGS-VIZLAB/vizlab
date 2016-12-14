a7141ff19da2ca916455aafe9573c7e35be8adde
Lindsay Carr <lcarr@usgs.gov>
2016-12-14 09:15:46 -0600

    thumbnails will work now??

--- R/publishLandingPage.R
+++ R/publishLandingPage.R
@@ -66,7 +66,8 @@ getVizInfo <- function(org, repo){
                             template="templates/vizzies.mustache",
                             publisher="section",
                             context=list(name=viz_info$name,
-                                         thumbnail=getVizUrl(viz_info$`thumbnail-landing`$url),
+                                         thumbnail=file.path(getVizUrl(viz_info$path),
+                                                             getVizUrl(viz_info$`thumbnail-landing`$url)),
                                          alttext=viz_info$`thumbnail-landing`$alttext,
                                          path=getVizUrl(viz_info$path),
                                          description=viz_info$description))
