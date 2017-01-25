b3f975d65982103034ab83e03325648af117189b
Jordan Walker <jiwalker@usgs.gov>
2017-01-25 16:38:12 -0600

    Switching to new signature and a couple yaml fixes

--- inst/testviz/scripts/visualize/relativeAbundance.R
+++ inst/testviz/scripts/visualize/relativeAbundance.R
@@ -1,13 +1,12 @@
 #' My visualizer function
-#' 
+#'
 #' Could also accept these args: viz.id, data.info. but we're not using them so
 #' won't list them.
-visualize.relativeAbundance <- function(
-  CuyahogaShort=readData('CuyahogaShort'), 
-  MayflyNymph=readData('MayflyNymph'), 
-  outfile='figures/relativeAbundanceFig.svg', ...) {
-  
+visualize.relativeAbundance <- function(viz) {
+  outfile <- viz[["location"]]
+  data <- readDepends(viz)
+
   svg(outfile, height=4, width=4)
-  plot(Above ~ Below, MayflyNymph)
+  plot(Above ~ Below, data[["MayflyNymph"]])
   dev.off()
 }
