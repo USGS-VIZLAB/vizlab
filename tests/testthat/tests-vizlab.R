066073c83da98c7a1c89c16851d0f6c11f332248
Alison Appling <aappling@usgs.gov>
2017-09-19 22:56:05 -0700

    update tests for new fetchTimestamp behaviors

--- tests/testthat/tests-vizlab.R
+++ tests/testthat/tests-vizlab.R
@@ -105,19 +105,48 @@ test_that("getting template from library works", {
 context("fetchTimestamps working")
 
 test_that(".url works", {
-  #a site with no modified tag
-  viz <- list(id="foo", remoteURL="www.google.com")
-  attr(viz , 'class') <- "url"
-  expect_true(fetchTimestamp(viz))
+  tsfile <- locateTimestampFile('foo')
+  expect_false(file.exists(tsfile))
+  
+  # a site with no modified tag
+  viz <- as.fetcher(as.viz(list(id="foo", remoteURL="www.google.com", fetcher="url")))
+  expect_error(fetchTimestamp(viz), "'last-modified' field not found in headers")
+  
+  # a site with a last-modified tag
+  viz <- as.fetcher(as.viz(list(id="foo", remoteURL="cran.r-project.org/web/packages/roxygen2/vignettes/markdown.html", fetcher="url")))
+  fetchTimestamp(viz)
+  expect_is(as.POSIXct(readLines(tsfile), tz='UTC'), 'POSIXct')
 })
 
 test_that("sciencebase works",{
-  #with no existing timestamp
-  expect_true(fetchTimestamp('Cuyahoga'))
+  tsfile <- locateTimestampFile('Cuyahoga')
+  expect_false(file.exists(tsfile))
   
-  #write the timestamp for the Cuyahoga file
-  writeTimestamp(as.POSIXct("2016-06-12 15:50:15 CDT"), as.viz("Cuyahoga"))
-  expect_false(fetchTimestamp('Cuyahoga'))
+  # with no existing timestamp, fetchTimestamp should create a file
+  fetchTimestamp('Cuyahoga')
+  expect_true(file.exists(tsfile))
+  ts1 <- readTimestamp('Cuyahoga')
+  # check against the known timestamp for the Cuyahoga file
+  expect_equal(ts1, as.POSIXct("2016-06-12 22:50:15", tz="UTC"))
+  
+  # fetch timestamp and expect the mtime and the ts contents to stay the same
+  mt1 <- file.mtime(tsfile)
+  fetchTimestamp('Cuyahoga')
+  mt2 <- file.mtime(tsfile)
+  expect_equal(mt1, mt2)
+  ts2 <- readTimestamp('Cuyahoga')
+  expect_equal(ts1, ts2)
+  
+  # but if oldtimestamp differs, fetchTimestamp should update the value
+  writeTimestamp(Sys.time(), 'Cuyahoga')
+  mt3 <- file.mtime(tsfile)
+  ts3 <- readTimestamp('Cuyahoga')
+  Sys.sleep(1)
+  fetchTimestamp('Cuyahoga')
+  mt4 <- file.mtime(tsfile)
+  ts4 <- readTimestamp('Cuyahoga')
+  expect_true(mt4 >= mt3)
+  expect_true(ts4 <= ts3)
 })
 
 test_that("unimplemented fetcher hits timestamp.fetcher", {
