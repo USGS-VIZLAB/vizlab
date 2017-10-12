2f7a28390cb43b39ab6bf049b780966fe34c9b5a
Jordan S Read <jread@usgs.gov>
2017-10-11 21:43:56 -0500

    convert tooltip to rough d3

--- inst/js/tooltip.js
+++ inst/js/tooltip.js
@@ -2,38 +2,35 @@ var hoverTimer = null;
 var hoverDelay = 400; //ms
   
 function hovertext(text, evt){
-  var tooltip = document.getElementById("tooltip-text");
-  var tooltip_bg = document.getElementById("tooltip-box");    
-  var tool_pt = document.getElementById("tooltip-point");
+  var tipG = d3.selectAll('#tooltip-group');
+  if (tipG.empty()) {
+    console.error("a #tooltip-group <g/> element is required");
+  }  
+  // remove all children of the tooltip group:
+  tipG.selectAll("*").remove();
+  
   if (evt === undefined){
     if(hoverTimer) {
       clearTimeout(hoverTimer); //stop when off area
     }
-    tooltip.firstChild.data = ' ';
-    tooltip_bg.setAttribute("class","hidden");
-    tooltip_bg.setAttribute("x",0);
-    tool_pt.setAttribute("class","hidden");
   } else {
+    var tooltip_bg = tipG.append('rect').attr("id", "tooltip-box").attr("height", 24).attr("class", "tooltip-box");
+    var tool_pt = tipG.append('path').attr("id", "tooltip-point").attr("d", "M-6,-11 l6,10 l6,-11").attr("class","tooltip-box");
+    var tooltip = tipG.append('text').attr("id", "tooltip-text").attr("dy","-1.1em").attr('text-anchor',"middle").attr("class","tooltip-text-label svg-text").text(text);
     pt = cursorPoint(evt);
     pt.x = Math.round(pt.x);
     pt.y = Math.round(pt.y);
     var svgWidth = Number(svg.getAttribute("viewBox").split(" ")[2]);
-    tooltip.setAttribute("x",pt.x);
-    tooltip.setAttribute("y",pt.y);
-    tooltip.firstChild.data = text;
-    var length = Math.round(tooltip.getComputedTextLength());
+    tooltip.attr("x",pt.x).attr("y",pt.y);
+    var length = Math.round(tooltip.node().getComputedTextLength());
     if (pt.x - length/2 - 6 < 0){
-      tooltip.setAttribute("x",length/2+6);
+      tooltip.attr("x",length/2+6);
     } else if (pt.x + length/2 + 6 > svgWidth) {
-      tooltip.setAttribute("x", svgWidth-length/2-6);
+      tooltip.attr("x", svgWidth-length/2-6);
     }
-    tool_pt.setAttribute("transform","translate("+pt.x+","+pt.y+")");
-    tooltip_bg.setAttribute("x",tooltip.getAttribute("x")-length/2-6);
-    tooltip_bg.setAttribute("y",pt.y-35);
-    tooltip.setAttribute("class","shown");
-    tooltip_bg.setAttribute("class","tooltip-box");
-    tool_pt.setAttribute("class","tooltip-box");
-    tooltip_bg.setAttribute("width", length+12);
+    tool_pt.attr("transform","translate("+pt.x+","+pt.y+")");
+    tooltip_bg.attr("x", tooltip.attr("x")-length/2-6).attr("y",pt.y-35).attr("class","tooltip-box").attr("width", length+12);
+
     if(hoverTimer){
       clearTimeout(hoverTimer);
     }
