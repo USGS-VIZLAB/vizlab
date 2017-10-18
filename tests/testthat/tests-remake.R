f60badbba3d31de0bfbeb341fcc22d6a90455430
Alison Appling <aappling@usgs.gov>
2017-10-18 16:24:45 -0700

    tests!

---
+++ tests/testthat/tests-remake.R
@@ -0,0 +1,42 @@
+context("remake")
+
+oldwd <- getwd()
+#these tests need to use the test viz
+testtmp <- setup(copyTestViz=TRUE)
+
+test_that('createRemakefile makes remake.yaml', {
+  vizlab:::createRemakefile()
+  # the file should get made
+  expect_true(file.exists('remake.yaml'))
+  
+  # the file should contain typical top-level remake components
+  expect_equal(
+    grep('^([[:alpha:]].*):', readLines('remake.yaml'), value=TRUE), 
+    c('target_default: Viz', 'packages:', 'sources:', "targets:"))
+  
+  # the file should contain typical vizlab concepts and phases
+  expect_equal(
+    grep('# --- (.*) --- #', readLines('remake.yaml'), value=TRUE), 
+    paste0('  # --- ', c('parameter','fetch','process','visualize','publish','resource','scripts','Job groups'), ' --- #'))
+})
+
+test_that('prepSources can combine and subset scripts', {
+  # can combine scripts
+  prepSources('scripts/read/myCsvReader.R', 'scripts/fetch/mayflyNymph.R', outfile='vizlab/remake/scripts/prep1.R')
+  prep1 <- readLines('vizlab/remake/scripts/prep1.R')
+  expect_true(any(grepl('readData\\.myCsvReader', prep1)))
+  expect_true(any(grepl('fetch\\.mayflyNymph', prep1)))
+  expect_true(any(grepl('fetchTimestamp\\.mayflyNymph', prep1)))
+  
+  # removes comments and whitespace
+  expect_false(any(grepl('#', prep1)))
+  expect_false(any(prep1 == ''))
+  
+  # can subset to specific functions
+  prepSources('scripts/fetch/mayflyNymph.R', functions='fetch.mayflyNymph', outfile='vizlab/remake/scripts/prep2.R')
+  prep2 <- readLines('vizlab/remake/scripts/prep2.R')
+  expect_true(any(grepl('fetch\\.mayflyNymph', prep2)))
+  expect_false(any(grepl('fetchTimestamp\\.mayflyNymph', prep2)))
+})
+
+cleanup(oldwd, testtmp)
