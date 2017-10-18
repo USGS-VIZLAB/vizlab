5c98333006eceae50e7ab7eb5d52db6bd9e68af4
jzwart <jzwart@usgs.gov>
2017-10-18 16:31:28 -0500

    fixing .shp reader, rgdal to suggested packages

--- R/readData.R
+++ R/readData.R
@@ -229,7 +229,7 @@ readData.shp <- function(viz){
   
   unzip(viz[['location']], exdir = shp.path)
   layer <- tools::file_path_sans_ext(list.files(shp.path, pattern='*.shp'))[1]
-  data.out = readOGR(shp.path, layer=layer)
+  data.out <- rgdal::readOGR(shp.path, layer=layer)
   unlink(shp.path, recursive = TRUE)
   return(data.out)
 }
