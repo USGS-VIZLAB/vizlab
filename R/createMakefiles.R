d251dfb43c0cc99a276951b1cb775b0fac030ad4
Alison Appling <aappling@usgs.gov>
2017-10-30 10:35:56 -0700

    move updateConfigInfoFile out of make-centric script

--- R/createMakefiles.R
+++ R/createMakefiles.R
@@ -168,34 +168,6 @@ createConfigCall <- function(){
 }
 
 
-#' updateConfigInfoFile
-#' 
-#' Creates 1 file per viz id with the content info. This is necessary to 
-#' update the project if only the viz.yaml is updated (otherwise make)
-#' won't trigger an update.
-#' 
-#' @param viz.id character[s] giving specific IDs of items to return, or missing
-#'   to return all content items
-#'   
-#' @export
-updateConfigInfoFile <- function(viz.id){
-  
-  info <- getContentInfo(viz.id)
-  
-  file.config <- paste0(viz.id,".rds")
-  full.config <- file.path("vizlab/make/config",file.config) 
-  if(file.exists(full.config)){
-    orig.info <- readRDS(full.config)
-    if(!isTRUE(all.equal(orig.info, info))) {
-      saveRDS(info, full.config)
-    }
-  } else {
-    saveRDS(info, full.config)
-  }
-  
-}
-
-
 #' Create the macros section
 #'
 #' Create a character string defining the macros to include in every makefile
